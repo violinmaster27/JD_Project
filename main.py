016b9f88d753acae0fbb5d24888fd20f0bd8dd57
Joseph Duque <josephduque27@yahoo.com>
2020-10-16 15:27:27 -0500

    message

--- main.py
+++ main.py
@@ -1,128 +1,303 @@
-# the main menu that user will see, they will choose which room to enter
-USER_CHOICE = """
-You have now entered the house. You must choose which room to enter.
-    - '1' to enter the dining room
-    - '2' to enter the living room
-    - '3' to enter the kitchen
-    - '4' to enter the bathroom
-    - '5' to enter the bedroom
-    - 'q' to quit
-
-Enter your choice here: """
-
-# empty list of room completed, this will fill up
-# each time a room has been completed
+# ************ GAME STATE ************
+# Set initial room
+current_room = "front_door"
 rooms_completed = []
-
-
-# each room will have its own function with challenges 'inside' each room
-
-def dining_room():
-    if '1' in rooms_completed:
-        print("You have already completed the dining room.")
-    else:
-        print("You have entered the dining room.")
-        room_input = input("Will you enter the kitchen or the bedroom? (3/5): ")
-
-        if room_input == '3':
-            kitchen()
-        elif room_input == '5':
-            bedroom()
-        else:
-            print("You cannot enter that room right now.")
-            dining_room()
-
-def living_room():
-    if '2' in rooms_completed:
-        print("You have already completed the living room.")
-    else:
-        print("You have entered the living room.")
-        room_input = input("Will you enter the kitchen or the bathroom? (3/4): ")
-
-        if room_input == '3':
-            kitchen()
-        elif room_input == '4':
-            bathroom()
-        else:
-            print("You cannot enter that room right now.")
-            living_room()
-
-def kitchen():
-    if '3' in rooms_completed:
-        print("You have already completed the kitchen.")
-    else:
-        print("You have entered the kitchen.")
-        room_input = input("Will you enter the dining room, living room, or the bedroom? (1/2/5): ")
-
-        if room_input == '1':
-            dining_room()
-        elif room_input == '2':
-            living_room()
-        elif room_input == '5':
-            bedroom()
-        else:
-            print("You cannot enter that room right now.")
-            kitchen()
-
-def bathroom():
-    if '4' in rooms_completed:
-        print("You have already completed the bathroom.")
-    else:
-        print("You have entered the bathroom.")
-        room_input = input("Will you enter the living room or the bedroom? (2/5): ")
-
-        if room_input == '2':
-            living_room()
-        elif room_input == '5':
-            bedroom()
-        else:
-            print("You cannot enter that room right now.")
-            bathroom()
-
-def bedroom():
-    if '5' in rooms_completed:
-        print("You have already completed the bedroom.")
-    else:
-        print("You have entered the bedroom.")
-        room_input = input("Will you enter the dining room, kitchen, or bathroom? (1/3/4): ")
-
-        if room_input == '1':
-            dining_room()
-        elif room_input == '3':
-            kitchen()
-        elif room_input == '4':
-            bathroom()
-        else:
-            print("You cannot enter that room right now.")
-            bedroom()
-
-
-def main():
-    # dictionary of room options that will call the room function
-    room_choices = {
-        '1': dining_room,
-        '2': living_room,
-        '3': kitchen,
-        '4': bathroom,
-        '5': bedroom
+all_rooms = [
+    {
+        'name': 'front_door',
+        'room_description': 'The front door is a massive plank of dark oak, with a large golden handle.',
+        'room_actions': ['Try the front door.', 'Look up the stairs.', 'Hide inside the coat closet'],
+        'action_results': ['The door is locked shut. It will not budge.', 'Darkness...you hear faint footsteps on one of the upper floors.', 'You are hidden. You also find a spare tuxedo hanging in the closet, obviously the person who lived here liked to play football.']
+    },
+    {
+        'name': 'dining_room',
+        'room_description': 'A long table sits at the center of the dining room, perfectly set with pristine china. A large chandelier floats above it, candlelight illuminating the small room with a warm glow.',
+        'room_actions': ['Sit at the table.', 'Hide under the table.', 'Inspect the chandelier.'],
+        'action_results': ['The seat is warm.', 'The table cloth conseales you... You find a message carved on the underside of the table.', 'It is rusty.']
+    },
+    {
+        'name': 'living_room',
+        'room_description': 'Surprisingly, the living room seems well kept. Eveything is in its right place. A large painting is hung on the wall. A painting of some man, looks familiar from somewhere...',
+        'room_actions': ['Lift the painting', 'Sit on the lounge sofa', 'Inspect the painting'],
+        'action_results': ['You lift the painting to find some sort of safe behind it. If only you had the combination...', 'Creeek! Sitting on the sofa causes a loud noise.', 'You see initials on the bottom left of the painting: A.A. Probably the persons initials?']
+    },
+    {
+        'name': 'kitchen',
+        'room_description': "You try to turn on the light but it doesn't work, maybe that's for the better. Still, the kitchen has a strange odor to it, and some dishes in the sink.",
+        'room_actions': ['Check inside the fridge.', 'Look inside the cabinet.', 'Open the drawer next to the sink.'],
+        'action_results': ['A putrid stench emits from the fridge, and you notice a cloudy jar on the top shelf.', 'Nothing special in here.', 'You find a sheathed knife lying in the drawer. It has some Asain writing on it.']
+    },
+    {
+        'name': 'bathroom',
+        'room_description': 'The bathroom is small. Not a pleasant smell at all, and the light flickers for a while before fully turning on.',
+        'room_actions': ['Flush the toilet.', 'Open the medicine cabinet.', 'Pull back the shower curtain.'],
+        'action_results': ['It seems broken...I guess the smell is staying.', 'A lot of painkillers...and fungicide, fungus???', 'It is as you feared, a small pool of blood remains in the tub.']
+    },
+    {
+        'name': 'bedroom',
+        'room_description': 'A very simple looking bedroom. Signs of use appear throughout the room. But where is the resident?',
+        'room_actions': ['Look in the Closet', 'Search the Nightstand', 'Look in the mirror'],
+        'action_results': ["It's not a closet at all, but a secret passageway leading up somehwere...", 'Fitting, you find a copy of The Shining.', 'You see a figure pass by the door behind you...is it too late to leave?']
     }
+]
 
-    user_input = input(USER_CHOICE)
+# Any time actions
+def prompt_action():
+    USER_CHOICE = """
+    Change rooms, look around, or give up. What do you do?
+        - '1' to change rooms
+        - '2' to look around
+        - 'q' to give up (quit the game)
 
-    # if user does not choose to quit
-    while user_input != 'q':
-        # check if it's a valid room
-        if user_input in ('1', '2', '3', '4', '5'):
-            # call that room's function
-            room_fuction = room_choices[user_input]
-            room_fuction()
-        else:
-            print("Not a valid input. Please choose a valid room.")
-            # prompt user for choice again
-            user_input = input(USER_CHOICE)
+    Enter your choice here: """
+    return USER_CHOICE
 
-    print("Quitting game.")
+# need to break after calling change_room() or look_around() inside and loops or stuff will break, no pun intended
+def change_room(room):
+    eval(room + '()')
+
+def look_around(room_index):
+    room = all_rooms[room_index]
+
+    print(room.get('room_description'))
+
+    action_options = ""
+    for index, action in enumerate(room.get('room_actions')):
+        action_options = action_options + f'- {index + 1} to...' + action + '\n'
+    # extra line to add this last line is kind of redundant
+    action_options = action_options + 'Enter your choice here: '
+
+    USER_CHOICE = input(action_options)
+    print(room.get('action_results')[int(USER_CHOICE) - 1])
+
+    # call room function again to continue, obviously this shouldnt happen if the player is hidden but we'll deal with that soon
+    change_room(current_room)
+
+
+def end_game():
+    print("GAME OVER")
     quit()
 
+# ************ ROOM FUNCTIONS ************
 
-main()
+def front_door():
+    global current_room
+    current_room = "front_door"
+
+    print("You are at the front door. The stairway is blocked by debris.")
+
+    user_input = input(prompt_action())
+
+    while True:
+        if user_input == '1':
+            room_input = input("""
+                Will you enter the living room or the dining room? (L/D):
+                I changed my mind. (nvm)
+                """).upper()
+            if room_input == 'L':
+                change_room('living_room')
+                break
+            elif room_input == 'D':
+                change_room('dining_room')
+                break
+            elif room_input == 'NVM':
+                front_door()
+            else:
+                print("Not a valid option. Choose again.")
+                continue
+        elif user_input == '2':
+            look_around(0)
+            break
+        elif user_input == 'q':
+            end_game()
+        else:
+            print("Not a valid input. Please choose a valid action.")
+            user_input = input(prompt_action())
+
+def dining_room():
+    global current_room
+    current_room = "dining_room"
+
+    print("You are in the dining room.")
+
+    user_input = input(prompt_action())
+
+    while True:
+        if user_input == '1':
+            room_input = input("""
+                Will you enter the kitchen or the bedroom? (K/BR):
+                I changed my mind. (nvm)
+                """).upper()
+            if room_input == 'K':
+                change_room('kitchen')
+                break
+            elif room_input == 'BR':
+                change_room('bedroom')
+                break
+            elif room_input == 'NVM':
+                dining_room()
+            else:
+                print("Not a valid option. Choose again.")
+                continue
+        elif user_input == '2':
+            look_around(1)
+            break
+        elif user_input == 'q':
+            end_game()
+        else:
+            print("Not a valid input. Please choose a valid action.")
+            user_input = input(prompt_action())
+
+
+def living_room():
+    global current_room
+    current_room = "living_room"
+
+    print("You are in the living room.")
+
+    user_input = input(prompt_action())
+
+    while True:
+        if user_input == '1':
+            room_input = input("""
+                Will you enter the kitchen or the bathroom? (K/BTH):
+                I changed my mind. (nvm)
+                """).upper()
+            if room_input == 'K':
+                change_room('kitchen')
+                break
+            elif room_input == 'BTH':
+                change_room('bathroom')
+                break
+            elif room_input == 'NVM':
+                living_room()
+            else:
+                print("Not a valid option. Choose again.")
+                continue
+        elif user_input == '2':
+            look_around(2)
+            break
+        elif user_input == 'q':
+            end_game()
+        else:
+            print("Not a valid input. Please choose a valid action.")
+            user_input = input(prompt_action())
+
+
+def kitchen():
+    global current_room
+    current_room = "kitchen"
+
+    print("You are in the kitchen.")
+
+    user_input = input(prompt_action())
+
+    while True:
+        if user_input == '1':
+            room_input = input("""
+                Will you enter the dining room, living room, or bedroom? (D/L/BR):
+                I changed my mind. (nvm)
+                """).upper()
+            if room_input == 'D':
+                change_room('dining_room')
+                break
+            elif room_input == 'L':
+                change_room('living_room')
+                break
+            elif room_input == 'BR':
+                change_room('bedroom')
+                break
+            elif room_input == 'NVM':
+                kitchen()
+            else:
+                print("Not a valid option. Choose again.")
+                continue
+        elif user_input == '2':
+            look_around(3)
+            break
+        elif user_input == 'q':
+            end_game()
+        else:
+            print("Not a valid input. Please choose a valid action.")
+            user_input = input(prompt_action())
+
+
+def bathroom():
+    global current_room
+    current_room = "bathroom"
+
+    print("You are in the bathroom.")
+
+    user_input = input(prompt_action())
+
+    while True:
+        if user_input == '1':
+            room_input = input("""
+                Will you enter the living room or the bedroom? (L/BR):
+                I changed my mind. (nvm)
+                """).upper()
+            if room_input == 'L':
+                change_room('living_room')
+                break
+            elif room_input == 'BR':
+                change_room('bedroom')
+                break
+            elif room_input == 'NVM':
+                bathroom()
+            else:
+                print("Not a valid option. Choose again.")
+                continue
+        elif user_input == '2':
+            look_around(4)
+            break
+        elif user_input == 'q':
+            end_game()
+        else:
+            print("Not a valid input. Please choose a valid action.")
+            user_input = input(prompt_action())
+
+
+def bedroom():
+    global current_room
+    current_room = "bedroom"
+
+    print("You are in the bedroom.")
+
+    user_input = input(prompt_action())
+
+    while True:
+        if user_input == '1':
+            room_input = input("""
+                Will you enter the dining room, kitchen, or bathroom? (D/K/BTH):
+                I changed my mind. (nvm)
+                """).upper()
+            if room_input == 'D':
+                change_room('dining_room')
+                break
+            elif room_input == 'K':
+                change_room('kitchen')
+                break
+            elif room_input == 'BTH':
+                change_room('bathroom')
+                break
+            elif room_input == 'NVM':
+                bedroom()
+            else:
+                print("Not a valid option. Choose again.")
+                continue
+        elif user_input == '2':
+            look_around(5)
+            break
+        elif user_input == 'q':
+            end_game()
+        else:
+            print("Not a valid input. Please choose a valid action.")
+            user_input = input(prompt_action())
+
+
+# ************ GAME START ************
+def start_game():
+    print("You have now entered the house. The front door locks behind you! The house is silent but ominous.")
+    change_room(current_room)
+
+start_game()
